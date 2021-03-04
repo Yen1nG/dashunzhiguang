816a45f18226caf7b28bbe64205eb72258cfd534
Yen1nG <wangyening1213@hotmail.com>
2021-03-03 18:11:53 -0800

    project organized

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Slideshow from './slideShow.js';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Header from './Header.js';
 import BGI from './images/BGI.jpg';
@@ -13,22 +12,22 @@ import { Player } from 'video-react';
 import newsvideo from './images/newsvideo.mp4';
 import videoposter from './images/videoposter.jpg';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//   },
+// }));
 
 export default function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <div className={classes.root}  style={{'backgroundColor': '#FFFFFF'}}>
+    <div style={{'backgroundColor': '#FFFFFF'}}>
       <Grid container spacing={0}>
         <Grid item md={12} sm={12} xs={12} style={{'paddingBottom': 0}}>
           <Header />
@@ -41,19 +40,19 @@ export default function App() {
         <Grid item md={1} sm={1} xs={1} style={{'paddingRight': 0}}></Grid>
 
         <Grid item md={1} sm={1} xs={1}></Grid>
-        <Grid item md={5} sm={12} xs={12} style={{'backgroundColor': '#856d72', 'paddingRight': '80px', 'paddingLeft': '80px', 'paddingTop': '7%', 'marginTop': '10px', 'marginBottom': '10px'}}>
-          <p style={{ fontSize: 22, textIndent: '2em', color: 'rgb(220, 220, 220)', 'lineHeight': 1.8}}>
+        <Grid item md={5} sm={12} xs={12} className='shunIntro' style={{'backgroundColor': '#856d72', 'margin': '10px 0'}}>
+          <p style={{ fontSize: 22, textIndent: '2em', color: 'rgb(220, 220, 220)', 'lineHeight': 1.8, 'paddingRight': '40px', 'paddingLeft': '40px', 'paddingTop': '20%', 'marginTop': '10px', 'textAlign': 'justify'}}>
             约四千三百年以前，舜出生于山西省垣曲县境内的诸冯山。孟子曰：“舜生于诸冯，
             迁于负夏。” 《史记·五帝本纪录》载：“舜耕历山，渔雷泽，陶河滨，作什器于寿丘，
             就时于负夏。” 《水经注》记：“教水南流历鼓钟山峡，悬洪五丈，飞流注壑，北四十里
             大舜浚井在焉。东有大阜为瞽冢，南有诸冯，北有历山，东南有雷泽，东北有耕田遗迹。“
             舜青少年时期曲尽孝道的感人故事，在垣曲民间千古流传。因此，垣曲素有”舜乡”之称。
           </p>
-          <p style={{ fontSize: 20, textIndent: "2em", color: "rgb(220, 220, 220)", textAlign: "right"}}>
+          <p style={{ fontSize: 20, textIndent: "2em", color: "rgb(220, 220, 220)", textAlign: "right", 'paddingRight': '40px' }}>
             ——垣曲舜文化研究会会长 李道义
           </p>
         </Grid>
-        <Grid item md={5} sm={12} xs={12} style={{'marginTop': '10px', 'marginBottom': '10px'}}>
+        <Grid item md={5} sm={12} xs={12} style={{'margin': '10px 0'}}>
           <img src={shunwood} style={{'display': 'block', 'width': '100%', 'height': '100%'}} className="shunds" title="大舜之光" alt="大舜之光"/>
         </Grid>
         <Grid item md={1} sm={1} xs={1}></Grid>
